d88c3295912869ca7c18834f6db733c102e36a77
Alisher Musayev <71883135+Alisher1994@users.noreply.github.com>
2025-12-26 23:17:26 +0500

    Delete Другой проект с функционалом Фейс ИД directory

--- Другой проект с функционалом Фейс ИД/football_school/migrate_db.py
+++
@@ -1,7 +0,0 @@
-from backend.models.models import db
-from app import app
-
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    print('✅ База данных пересоздана с новыми полями payment_month и payment_year')
